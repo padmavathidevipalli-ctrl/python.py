622e2f669245bb3011be7493ffedbd320bc21f14
padmavathidevipalli-ctrl <padmavathidevipalli@gmail.com>
2025-09-08 11:15:39 +0530

    python different operators are in

---
+++ operators.py
@@ -0,0 +1,26 @@
+#presedence operators
+''''
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+c=int(input("enter a number"))
+d=int(input("enter a number"))
+print(a+b-c*d)
+print(a%b+c)
+print(a*b==c)
+'''
+'''
+#bitwise operators
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+print(a>>b)
+print(b<<a)
+print(a&b)
+print(a|b)
+'''
+#assignment operators
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+print(a=a+2)
+print(b+=a)
+print(a*=b)
+
